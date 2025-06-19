2e65f518cd229faebcad8433cb7da59e242dfc0e
isaaclins <isaac.lins07@gmail.com>
2025-06-19 23:57:23 +0200

    feat: add password reset functionality with new models, repository, and service implementation

---
+++ 183_12_1_tresorbackend_rupe-master/src/main/java/ch/bbw/pr/tresorbackend/repository/PasswordResetTokenRepository.java
@@ -0,0 +1,10 @@
+package ch.bbw.pr.tresorbackend.repository;
+
+import ch.bbw.pr.tresorbackend.model.PasswordResetToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface PasswordResetTokenRepository extends JpaRepository<PasswordResetToken, Long> {
+    Optional<PasswordResetToken> findByToken(String token);
+}
